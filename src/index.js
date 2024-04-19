bd3fc4c32a8c6a7c39ca310bb0c63aec2fd254f8
kuciapakrystian <kuciapakrystian@outlook.com>
2024-04-19 20:13:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./components/GlobalStyle";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = {
+  colors: {
+    mainBgColor: "#ced4da",
+    textColor: "#050505",
+    contactBtn: "#2982ff",
+    deleteBtn: "#ff2929",
+  },
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
